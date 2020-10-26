a129fab9c3634beec9849985b9aa012f017f65c2
Ramiro-luengo <ramirobmds@gmail.com>
2020-10-25 21:49:04 -0300

    Agrego parser digitos

--- scala/src/Pruebas/Parsers.scala
+++ scala/src/Pruebas/Parsers.scala
@@ -4,10 +4,12 @@ import scala.util.{Try,Success,Failure}
 trait Parsers {
   
    def anyChar(cadena:String): Try[Char] = {
-    Try(cadena match {
-      case "" => throw new Exception();
-      case _ => cadena.head;
-    })
+      Try{
+          cadena match {
+          case "" => throw new Exception();
+          case _ => cadena.head;
+          }
+    }
   }
   
   def char(caracter: Char, cadena:String): Try[Char] = {
@@ -20,6 +22,26 @@ trait Parsers {
     }
   }
   
+   def string(cadenaFiltro:String, cadenaAParsear:String): Try[String] = {
+		   Try{
+  			   cadenaAParsear match {
+  			   case "" => throw new Exception();
+  			   case c if c contains cadenaFiltro => cadenaFiltro;
+  			   case _ => throw new Exception();
+  			   }
+		   }
+		   
+   }
+   
+   def digit(caracter:Char): Try[Int] = {
+		   Try{
+		     caracter match {
+		       case c if c.isDigit => c.asDigit;
+		       case _ => throw new Exception();
+		     }
+		   }
+   }
+		   
   def num(numero:Int , lista: List[Int]): Int = {
     lista match {
       case Nil => throw new Exception();
@@ -27,26 +49,6 @@ trait Parsers {
       case _ =>  throw new Exception();
     }
   }
-  def string(cadenaFiltro:String, cadenaAParsear:String): Try[String] = {
-      Try{
-          cadenaAParsear match {
-            case "" => throw new Exception();
-            case c if c contains cadenaFiltro => cadenaFiltro;
-            case _ => throw new Exception();
-          }
-      
-      }
     
-  }
-  def digit(caracter:Char): Try[Int] = {
-
-    Try{
-
-      caracter.asDigit()
-    }
-    
-  }
-    
-  
   def num1 = num(1,_:List[Int]);
 }
