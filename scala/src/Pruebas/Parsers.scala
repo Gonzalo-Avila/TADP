d4b9881436f54e79386b4fa0dc9a57bb8ffb3928
Ramiro-luengo <ramirobmds@gmail.com>
2020-10-25 21:28:33 -0300

    Agrego parsers varios

--- scala/src/Pruebas/Parsers.scala
+++ scala/src/Pruebas/Parsers.scala
@@ -1,21 +1,24 @@
-
+import scala.io.Source
+import scala.util.{Try,Success,Failure}
 
 trait Parsers {
   
-   def anyChar(cadena:String): Char = {
-    cadena match {
+   def anyChar(cadena:String): Try[Char] = {
+    Try(cadena match {
       case "" => throw new Exception();
       case _ => cadena.head;
-    }
+    })
   }
   
-  /*def char(caracter: Char, cadena:String): Char = {
-    cadena match {
-      case "" => throw new Exception();
-      case caracter::tail  => cadena.head;
-      case _ => throw new Exception();
+  def char(caracter: Char, cadena:String): Try[Char] = {
+    Try{
+        cadena match {
+        case "" => throw new Exception();
+        case c if c.head == caracter => c.head;
+        case _ => throw new Exception();
+        }
     }
-  }*/
+  }
   
   def num(numero:Int , lista: List[Int]): Int = {
     lista match {
@@ -24,6 +27,26 @@ trait Parsers {
       case _ =>  throw new Exception();
     }
   }
+  def string(cadenaFiltro:String, cadenaAParsear:String): Try[String] = {
+      Try{
+          cadenaAParsear match {
+            case "" => throw new Exception();
+            case c if c contains cadenaFiltro => cadenaFiltro;
+            case _ => throw new Exception();
+          }
+      
+      }
+    
+  }
+  def digit(caracter:Char): Try[Int] = {
+
+    Try{
+
+      caracter.asDigit()
+    }
+    
+  }
+    
   
   def num1 = num(1,_:List[Int]);
 }
