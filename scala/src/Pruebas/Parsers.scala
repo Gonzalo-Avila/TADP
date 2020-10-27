6070780da20893175f43fb8663d3b47740a7d91b
Ramiro-luengo <ramirobmds@gmail.com>
2020-10-27 13:12:17 -0300

    Agrego parser combinator RightMost y LeftMost

--- scala/src/Pruebas/Parsers.scala
+++ scala/src/Pruebas/Parsers.scala
@@ -1,52 +1,86 @@
 import scala.io.Source
 import scala.util.{Try,Success,Failure}
 
-trait Parser{
-  def apply(cadena:String): Try[Resultado[_]]
+trait Parser[T]{
+  def apply(cadena:String): Try[Resultado[T]]
   
-  def <|> (otroParser:Parser) = new Or(this,otroParser);
-  def <>  (otroParser:Parser) = new Concat(this,otroParser);
-  
+  def <|> (otroParser:Parser[T]) = new Or[T](this,otroParser)
+  def <>  (otroParser:Parser[T]) = new Concat[T](this,otroParser)
+  def ~> (otroParser:Parser[T]) = new RightMost(this,otroParser)
+  def <~ (otroParser:Parser[T]) = new LeftMost(this,otroParser)
 }
 
 class Resultado[T](elementoParseado:T, cadenaRestante:String){
-  def getElementoParseado = elementoParseado;
-  def getCadenaRestante = cadenaRestante;
+  def getElementoParseado = elementoParseado
+  def getCadenaRestante = cadenaRestante
 }
 
-class Or (parser1:Parser, parser2:Parser) extends Parser {
+class Or [T](parser1:Parser[T], parser2:Parser[T]) extends Parser[T] {
  
-  def apply (cadena:String): Try[Resultado[_]] = {
+  def apply (cadena:String): Try[Resultado[T]] = {
       
-      val resultadoParser1 = parser1.apply(cadena);
+      val resultadoParser1 = parser1.apply(cadena)
       resultadoParser1 match {
-      case Success(resultado) => resultadoParser1;
-      case Failure(error) => parser2.apply(cadena);
+      case Success(resultado) => resultadoParser1
+      case Failure(error) => parser2.apply(cadena)
       }
    }
 }
-
-class Concat (parser1:Parser, parser2:Parser) extends Parser {
+class Concat [T](parser1:Parser[T], parser2:Parser[T]) extends Parser [Tuple2[T,T]]{
   
-  def apply(cadena:String): Try[Resultado[_]] = {
-      val resultadoParser1 = parser1.apply(cadena);
+  def apply(cadena:String): Try[Resultado[Tuple2[T,T]]] = {
+      val resultadoParser1 = parser1.apply(cadena)
       resultadoParser1 match {
-        case Failure(errorEnParser1) => throw new Exception();
+        case Failure(errorEnParser1) => throw new Exception()
         case Success(resultado1) => {
-          val resultadoParser2 = parser2.apply(resultadoParser1.get.getCadenaRestante);
+          val resultadoParser2 = parser2.apply(resultadoParser1.get.getCadenaRestante)
           resultadoParser2 match {
-            case Failure(errorEnParser2) => throw new Exception();
+            case Failure(errorEnParser2) => throw new Exception()
             case Success(resultado2) => 
               Try(new Resultado((resultadoParser1.get.getElementoParseado,
                   resultadoParser2.get.getElementoParseado),
-                  resultadoParser2.get.getCadenaRestante));
+                  resultadoParser2.get.getCadenaRestante))
           }
         }
     }
   }
 }
 
-object anyChar extends Parser {
+class RightMost [T](parser1:Parser[T], parser2:Parser[T]) extends Parser[T]{
+      def apply(cadena:String): Try[Resultado[T]] = {
+          val resultadoParser1 = parser1.apply(cadena);
+          resultadoParser1 match {
+            case Failure(errorEnParser1) => throw new Exception();
+            case Success(resultado1) => {
+              val resultadoParser2 = parser2.apply(resultadoParser1.get.getCadenaRestante);
+              resultadoParser2 match {
+                case Failure(errorEnParser2) => throw new Exception();
+                case Success(resultado2) => 
+                  resultadoParser2;
+              }
+             }
+          }
+      }
+}
+
+class LeftMost[T](parser1:Parser[T], parser2:Parser[T]) extends Parser[T]{
+    def apply(cadena:String): Try[Resultado[T]] = {
+            val resultadoParser1 = parser1.apply(cadena);
+            resultadoParser1 match {
+                case Failure(errorEnParser1) => throw new Exception();
+                case Success(resultado1) => {
+                  val resultadoParser2 = parser2.apply(resultadoParser1.get.getCadenaRestante);
+                  resultadoParser2 match {
+                    case Failure(errorEnParser2) => throw new Exception();
+                    case Success(resultado2) => 
+                      resultadoParser1;
+                  }
+                }
+            }
+    }
+}
+
+object anyChar extends Parser[Char] {
   
    def apply (cadena:String): Try[Resultado[Char]] = {
       Try(
@@ -58,7 +92,7 @@ object anyChar extends Parser {
   }
 }
 
-class char (caracter: Char) extends Parser {
+class char (caracter: Char) extends Parser[Char] {
     def apply(cadena:String): Try[Resultado[Char]] = {
         Try (
           cadena match {
@@ -71,32 +105,32 @@ class char (caracter: Char) extends Parser {
 }
 
 
-class string (cadenaFiltro:String) extends Parser{
+class string (cadenaFiltro:String) extends Parser[String]{
   
   def apply(cadenaAParsear:String): Try[Resultado[String]] = {
-	  Try (
-  	    cadenaAParsear match {
-  		  case "" => throw new Exception();
-  		  case c if c startsWith cadenaFiltro => new Resultado(cadenaFiltro,cadenaAParsear.substring(cadenaFiltro.size));
-  		  case _ => throw new Exception();
-   	   }
-		)   
+    Try (
+        cadenaAParsear match {
+        case "" => throw new Exception();
+        case c if c startsWith cadenaFiltro => new Resultado(cadenaFiltro,cadenaAParsear.substring(cadenaFiltro.size));
+        case _ => throw new Exception();
+        }
+    )   
    }
 }
 
-object digit extends Parser {
+object digit extends Parser[Char]{
   
    def apply(cadena:String): Try[Resultado[Char]] = {
-		   Try(
-		     cadena match {
-		       case cad if cad.head.isDigit => new Resultado(cad.head,cad.tail);
-		       case _ => throw new Exception();
-		     }
-		   )
+       Try(
+         cadena match {
+           case cad if cad.head.isDigit => new Resultado(cad.head,cad.tail);
+           case _ => throw new Exception();
+         }
+       )
    }
 }
 
-object integer extends Parser {
+object integer extends Parser [Int]{
   def apply(cadena:String): Try[Resultado[Int]] = {
     Try(
       cadena match {
@@ -107,7 +141,6 @@ object integer extends Parser {
     )
   }
 }
-
 /*  
   def double(cadena:String): Try[Double] = {
     var cadenaSeparada = cadena.split(".")
