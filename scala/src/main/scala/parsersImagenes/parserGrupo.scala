f2093f16f895facdd42b13a709d5aaa694cea355
Gonzalo-Avila <goner@est.frba.utn.edu.ar>
2020-10-29 20:56:34 -0300

    Prueba

--- scala/src/main/scala/parsersImagenes/parserGrupo.scala
+++ scala/src/main/scala/parsersImagenes/parserGrupo.scala
@@ -7,13 +7,13 @@ import scala.util.Try
 
 object parserGrupo {
 
-  def apply(cadena: String): Try[Resultado[((List[String], List[String]), (List[String], List[String]))]] = {
+  /*def apply(cadena: String): Try[Resultado[((List[String], List[String]), (List[String], List[String]))]] = {
 
     val parserInicio = (string("grupo") <> char('(').map { char => char.toString }).map { tupla => Tuple2(List(tupla._1), List(tupla._2)) }
     val parserParametros = armarParserDeParametros
 
     (parserInicio <> parserParametros).apply(cadena)
-  }
+  }*/
 
 }
 
