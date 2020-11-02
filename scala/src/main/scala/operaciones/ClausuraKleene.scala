9962572bf128bc41dbf71a40f047732064e640d6
Gonzalo-Avila <goner@est.frba.utn.edu.ar>
2020-11-02 10:55:51 -0300

    Hago clausura de Kleene con recursividad

--- scala/src/main/scala/operaciones/ClausuraKleene.scala
+++ scala/src/main/scala/operaciones/ClausuraKleene.scala
@@ -6,13 +6,20 @@ import scala.util.{Failure, Success, Try}
 
 class ClausuraKleene[T](parserOriginal:Parser[T]) extends Parser[List[T]]{
 
-  def apply(cadena:String): Try[Resultado[List[T]]] = {
+  def parsear(resultadoParcial: Resultado[List[T]]): Resultado[List[T]] =
+    parserOriginal(resultadoParcial.getCadenaRestante)
+      .map{r => parsear(Resultado(resultadoParcial.getElementoParseado ++ List(r.getElementoParseado), r.getCadenaRestante ))}
+      .recoverWith {_ => Try(resultadoParcial)}.get
+
+  def apply(cadena: String): Try[Resultado[List[T]]] = Try(parsear(Resultado(List(),cadena)))
+
+  /*def apply(cadena:String): Try[Resultado[List[T]]] = {
     
     var listaParcial: List[T] = List()
     var cadenaParcial = cadena
     var seguir = true
 
-    //TODO - Reemplazar while por recursividad
+    //TODO - Reemplazar while por recursividad - DONE
     while (seguir){
         val resultadoActual = parserOriginal.apply(cadenaParcial)
         resultadoActual match {
@@ -24,5 +31,5 @@ class ClausuraKleene[T](parserOriginal:Parser[T]) extends Parser[List[T]]{
         }
     }
     Try(Resultado(listaParcial,cadenaParcial))
-  }
+  }*/
 }
