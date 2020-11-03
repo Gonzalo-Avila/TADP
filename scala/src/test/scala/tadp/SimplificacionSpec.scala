98cced67c491d0026f9da5866d6081c7ed414f62
NicoCollazo <48805854+NicoCollazo@users.noreply.github.com>
2020-11-03 10:04:45 -0300

    Agrego test de simplificacion. Ver comment.

--- scala/src/test/scala/tadp/SimplificacionSpec.scala
+++ scala/src/test/scala/tadp/SimplificacionSpec.scala
@@ -1,17 +1,17 @@
-package tadp
+/*package tadp
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import procesamiento.{ProcesadorTexto, SimplificadorAST}
 
 class SimplificacionSpec extends AnyFlatSpec with should.Matchers {
 
-  /* TODO// Nico | No me deja comparar Unit con String. Hace falta que imprimir devuelva Unit?
-                 | ¿Es correcto que la simplificación no tenga .end al final?
+TODO//Nico | Ver como extraer el String de imprimir()
 
   it should "simplificar traslacion redundante" in {
     val traslacionRedundante = ProcesadorTexto.apply("traslacion[0, 0](\n\t\tcirculo[0 @ 5, 10]\n)\n")
     val traslacionSimplificada = SimplificadorAST.simplificarArbol(traslacionRedundante)
     traslacionSimplificada.imprimir() shouldEqual "adapter.circle((0.0,5.0),10.0)"
   }
-   */
-}
+
+}*/
